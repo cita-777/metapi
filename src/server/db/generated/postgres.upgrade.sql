cbaadbfedb589713b6076681174092190c29a2dd
DeliciousBuding <DeliciousBuding@users.noreply.github.com>
2026-03-15 14:06:53 +0800

    chore(schema): regenerate generated artifacts

--- src/server/db/generated/postgres.upgrade.sql
+++ src/server/db/generated/postgres.upgrade.sql
@@ -1,3 +1,1 @@
-ALTER TABLE "model_availability" ADD COLUMN "is_manual" BOOLEAN DEFAULT false;
-ALTER TABLE "proxy_logs" ADD COLUMN "downstream_api_key_id" INTEGER;
-CREATE INDEX "proxy_logs_downstream_api_key_created_at_idx" ON "proxy_logs" ("downstream_api_key_id", "created_at");
+-- no schema changes detected for postgres
